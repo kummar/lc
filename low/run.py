09e94658b998ff98df34412580ac496b22e9f19d
karrysong <karrysong@tencent.com>
2020-05-19 19:46:48 +0800

    Two sum add

--- low/run.py
+++ low/run.py
@@ -1,21 +1,22 @@
 l1 = [2, 4, 3]
-l2 = [5, 6, 4]
+l2 = [1, 1, 4]
 
-l1_in = l1[::-1]
-l2_in = l2[::-1]
+# Get Length
+len_l1 = len(l1)
+len_l2 = len(l2)
 
-result_1 = 0
-for i in range(0, len(l1_in)):
-	print(l1_in[i])
-	result_1 += 10 ** (len(l1_in) - i - 1) * l1_in[i]
-	print(result_1)
-result_2 = 0
-for i in range(0, len(l2_in)):
-	print(l2_in[i])
-	result_2 += 10 ** (len(l2_in) - i - 1) * l2_in[i]
-	print(result_2)
-result = result_1 + result_2
+carry = 0
 answer = []
-for i in str(result):
-	answer.append(int(i))
+if len_l1 >= len_l2:
+	for i in range(0, len(l1)):
+		temp = l1[i] + l2[i] + carry
+		print(temp)
+		if temp >= 10:
+			result = 10 - temp + temp % 10
+			print(result)
+			answer.append(result)
+			carry += 1
+		else:
+			answer.append(temp)
+
 print(answer)
